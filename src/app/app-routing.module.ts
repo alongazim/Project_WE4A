8838bfc221506ed7db31a4835b6087edfdf44ee5
aqhareus <aqharchenudin@gmail.com>
2024-01-04 20:24:57 +0100

    Adding card to all categories and routing towards each category

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,14 @@ import { RegistrationComponent } from './registration/registration.component';
 import { CardComponent } from './card/card.component';
 import { HomeVendorComponent } from './home-vendor/home-vendor.component';
 import { AddProductComponent } from './add-product/add-product.component';
+import { MonitorComponent } from './monitor/monitor.component';
+import { MouseComponent } from './mouse/mouse.component';
+import { PrinterComponent } from './printer/printer.component';
+import { ProductComponent } from './product/product.component';
+import { SmartphoneComponent } from './smartphone/smartphone.component';
+import { SpeakerComponent } from './speaker/speaker.component';
+import { LaptopComponent } from './laptop/laptop.component';
+import { KeyboardComponent } from './keyboard/keyboard.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
@@ -13,9 +21,15 @@ const routes: Routes = [
   {path:'registration-form', component: RegistrationComponent}, 
   {path:'keyboard/:id',component: CardComponent},
   {path:'home-vendor',component: HomeVendorComponent},
-  {path:'addProduct',component: AddProductComponent} 
- 
- 
+  {path:'addProduct',component: AddProductComponent}, 
+  {path:'laptop',component: LaptopComponent},
+  {path:'keyboard',component: KeyboardComponent},
+  {path:'monitor',component: MonitorComponent},
+  {path:'mouse',component: MouseComponent},
+  {path:'printer',component: PrinterComponent},
+  {path:'product',component: ProductComponent},
+  {path:'smartphone',component: SmartphoneComponent},
+  {path:'speaker',component: SpeakerComponent}
   ];
 
 @NgModule({
